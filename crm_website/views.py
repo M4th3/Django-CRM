0400ec1ddee747688ca746769c5f8b07d05fcf28
mathe <matheuspb24@hotmail.com>
2023-12-14 18:12:23 -0300

    fourth commit

--- crm_website/views.py
+++ crm_website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecord
 from .models import Record
 
 # Create your views here.
@@ -48,4 +48,50 @@ def register_user(request):
  
         
     return render(request, 'register.html', {'form':form})
- 
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+
+    else:
+        messages.error(request, 'You must be logged in to accesss this page!!!') 
+        return redirect('homepage')  
+
+def delete_record(request, pk):
+    records = Record.objects.get(id=pk)
+    records.delete()
+    messages.success(request, 'The record has been deleted successfully')
+    return redirect('homepage')
+
+def add_record(request):
+    form = AddRecord()
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AddRecord(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'The record has been add with success!!!')
+                return redirect('homepage')  
+    else:
+        messages.error(request, 'You must been logged in!!')
+        return redirect('homepage')         
+    return render(request, 'add.html', {'form':form})
+
+def update_record(request,pk):
+    if request.user.is_authenticated:
+        current_data = Record.objects.get(id=pk)
+        form = AddRecord(instance=current_data)
+        if request.method == 'POST':
+            new_form = AddRecord(request.POST)
+            if new_form.is_valid():
+                new_form.save()
+            messages.success(request, 'Your record has been updated successfully!!!')
+            return redirect('homepage')
+        
+        return render(request, 'update.html', {'form': form, 'current_data':current_data})
+
+    else:
+        messages.error(request, 'You need to be logged in to update a record!!!')
+
+            
